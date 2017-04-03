ffa5ad9178165057d453bc04d18914655f501f00
Philip Putnam <putnamp989@gmail.com>
2017-04-03 16:30:16 -0700

    fix firebaselistobservables pipe, sort bug

--- src/app/calorie-sort.pipe.ts
+++ src/app/calorie-sort.pipe.ts
@@ -12,24 +12,19 @@ export class CalorieSortPipe implements PipeTransform {
 
     if (calories === undefined || operand === undefined) {
       return input;
-    } 
-
-
-    // if (calories === undefined || operand === undefined) {
-    //   return input;
-    // } else {
-    //   if (operand === 'greaterThan') {
-    //     return input.filter(function(food) {
-    //       return food.calories >= calories;
-    //     });
-    //   } else if (operand === 'lessThan') {
-    //     return input.filter(function(food){
-    //       return food.calories <= calories;
-    //     });
-    //   } else {
-    //     return input;
-    //   }
-    // }
+    } else {
+      if (operand === 'greaterThan') {
+        return input.filter(function(food) {
+          return food.calories >= calories;
+        });
+      } else if (operand === 'lessThan') {
+        return input.filter(function(food){
+          return food.calories <= calories;
+        });
+      } else {
+        return input;
+      }
+    }
   }
 
 }
